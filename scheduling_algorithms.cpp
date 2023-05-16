e1335369bda309b8e650b77df050f1fdceb92b6a
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2023-05-16 21:52:32 +0300

    Non-Preemptive Priority Algorithm is finished

--- scheduling_algorithms.cpp
+++ scheduling_algorithms.cpp
@@ -135,6 +135,34 @@ void priority_p(vector<Process>::const_iterator b, vector<Process>::const_iterat
     }
 }
 
+void priority_np(vector<Process>::const_iterator b, vector<Process>::const_iterator e, size_t n) {
+    vector<Process> processes(n);
+    copy(b, e, processes.begin());
+
+    int current_time = 0;
+    priority_queue<Process, vector<Process>, ComparePriority> pq;
+
+    int completed = 0;
+    int index = 0;
+
+    cout << "AT:CT\tPID\n";
+    while (completed < n) {
+        while (index < n && processes[index].arrival_time <= current_time)
+            pq.push(processes[index++]);
+
+        if (pq.empty()) {
+            cout << current_time << ':' << processes[index].arrival_time << "\tNULL" << '\n';
+            current_time = processes[index].arrival_time;
+            continue;
+        }
+
+        Process p = pq.top();
+        pq.pop();
+        ++completed;
+        cout <<  current_time << ':' << (current_time += p.burst_time) << '\t' <<p.process_number << '\n';
+    }
+}
+
 void rr(vector<Process>::const_iterator b, vector<Process>::const_iterator e , size_t n, int quantum) {
     vector<Process> processes(n);
     copy(b, e, processes.begin());
